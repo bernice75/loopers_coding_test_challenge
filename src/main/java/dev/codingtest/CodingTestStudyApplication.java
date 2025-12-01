d5212a2f5592cc0dd582606f7fb6dd5be5d110b7
mrkim <good12388@daum.net>
2025-12-01 20:32:54 +0900

    first commit

---
+++ src/main/java/dev/codingtest/CodingTestStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.codingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingTestStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingTestStudyApplication.class, args);
+    }
+
+}
